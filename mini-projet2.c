c0f9da84657d2265945dc4395a05f3ca5e05b65a
imanekr58 <imanekourdi92@gmaikl.com>
2025-09-08 11:51:35 +0200

    first commit

---
+++ mini-projet2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main () {
+    char  motCache [] = "hello" ;
+    int motAffiche = strlen (motCache) ;
+    printf ("le mot est : %s\n , mot") ;
+char motAffiche [] ; 
+for ( int x = 0 ; motAffiche< x ; x++) {
+motCache [x] ='----- ' ;
+
+printf (" saisir une laitre : ") ;
+motCache[strlen (mot) ] ='\0' ;
+
+
+    return 0;
+}
+ 
+
+
+
+
+
+}
