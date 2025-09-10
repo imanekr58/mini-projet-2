e4ec52c53c07462eb74e1628c86dd0a3877a2f37
imanekr58 <imanekourdi92@gmaikl.com>
2025-09-10 12:10:28 +0200

    chatbot

---
+++ mini-projet3/mini-projet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main () {
+    int i;
+   printf ("comment je peux t'aide:");
+   scanf ("%d", &i);
+   char mot;
+   int mot_trouve =0;
+   int entrer = 100;
+for (int a = 0; a <= mot ;a++)
+{
+    printf("%d",mot);
+    scanf ("%s", &mot);
+}
+char happy, sad, boren, tired;
+   if (i = happy) {
+    printf ("oh nice , i'm happy to for you%s",happy);
+   }
+  if (i = sad) {
+    printf("oh no! hope things get better soon%s ",sad);
+   }
+  if (i = boren) {
+    printf ("why, what do you do?");
+  }
+  if (i = tired) {
+    printf ("ok");
+  }
+  return 0;
+
+}
